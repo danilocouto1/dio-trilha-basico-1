0067777e0514854fe2198af4c97fe49d8f518eed
Dan <dansantosalfa@gmail.com>
2023-07-17 09:32:25 -0300

    Add - Entrada de dados via terminal

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -4,6 +4,14 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Digite o numero da conta: ");
+        int conta = scanner.nextInt();
+        System.out.println("Digite o numero da agencia: ");
+        int agencia = scanner.nextInt();
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.next();
+        System.out.println("Digite seu saldo: ");
+        double saldo = scanner.nextDouble();
 
         scanner.close();
 
